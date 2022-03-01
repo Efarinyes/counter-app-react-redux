3e95c35b27d9f6b003afd038db51236e731e4c39
Efarinyes <efarinyes@gmail.com>
2022-03-01 18:20:33 +0100

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  increaseCounter,
+  decreaseCounter,
+  reset,
+} from "./counter-app/actions/counterActions";
 
 function App() {
+  const count = useSelector((store) => store.counterReducer.count);
+  const dispatchCounter = useDispatch();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3> Counter app ( amb react-redux )</h3>
+        <p>{count}</p>
+        <br />
+        <button onClick={() => dispatchCounter(increaseCounter(1))}>+1</button>
+        <br />
+        <button onClick={() => dispatchCounter(increaseCounter(5))}>+5</button>
+        <br />
+        <button onClick={() => dispatchCounter(decreaseCounter(1))}>-1</button>
+        <br />
+        <button onClick={() => dispatchCounter(decreaseCounter(5))}>-5</button>
+        <br />
+        <button onClick={() => dispatchCounter(reset())}> Reset </button>
       </header>
     </div>
   );
